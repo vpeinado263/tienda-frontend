6918658fb2d5052489b161b36c31f0f88830264a
Victor Peinado <vpeinado263@gmail.com>
2024-07-22 12:20:56 -0300

    ahoa si

--- src/app/components/atoms/ProductosDisponiblesButton/ProductosDisponiblesButton.tsx
+++ src/app/components/atoms/ProductosDisponiblesButton/ProductosDisponiblesButton.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router';
 import styles from './ProductosDisponiblesButton.module.css';
 
 interface ProductosDisponiblesButtonProps {
@@ -9,41 +8,20 @@ interface ProductosDisponiblesButtonProps {
 
 const ProductosDisponiblesButton = ({ onClick }: ProductosDisponiblesButtonProps) => {
   const [productosDisponibles, setProductosDisponibles] = useState<number>(0);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
 
   useEffect(() => {
     axios.get('https://mi-back-end.onrender.com/products/count')
       .then(response => {
-        setProductosDisponibles(response.data.count); // Asume que la respuesta contiene la cantidad en la propiedad `count`
-        setLoading(false);
+        setProductosDisponibles(response.data.count);
       })
       .catch(error => {
-        setError('Error al obtener la cantidad de productos disponibles.');
-        setLoading(false);
         console.error('Error al obtener la cantidad de productos disponibles:', error);
       });
   }, []);
 
-  const handleClick = () => {
-    router.push('/products');
-    if (onClick) {
-      onClick();
-    }
-  };
-
   return (
-    <button className={styles.disponible} onClick={handleClick}>
-      {loading ? (
-        <span className={styles.span}>Cargando...</span>
-      ) : error ? (
-        <span className={styles.span}>{error}</span>
-      ) : (
-        <>
-          <span className={styles.span}>Productos Disponibles:</span> {productosDisponibles}
-        </>
-      )}
+    <button className={styles.disponible} onClick={onClick}>
+      <span className={styles.span}>Productos Disponibles</span> {productosDisponibles}
     </button>
   );
 };
