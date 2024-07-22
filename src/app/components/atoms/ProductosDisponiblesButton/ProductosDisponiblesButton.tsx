03ee1ac2caf4128d72b9efd4228638d1e322bed2
Victor Peinado <vpeinado263@gmail.com>
2024-07-22 01:37:22 -0300

    cambios

--- src/app/components/atoms/ProductosDisponiblesButton/ProductosDisponiblesButton.tsx
+++ src/app/components/atoms/ProductosDisponiblesButton/ProductosDisponiblesButton.tsx
@@ -8,24 +8,34 @@ interface ProductosDisponiblesButtonProps {
 
 const ProductosDisponiblesButton = ({ onClick }: ProductosDisponiblesButtonProps) => {
   const [productosDisponibles, setProductosDisponibles] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    
     axios.get('https://mi-back-end.onrender.com/products/count')
       .then(response => {
         setProductosDisponibles(response.data.count);
+        setLoading(false);
       })
       .catch(error => {
+        setError('Error al obtener la cantidad de productos disponibles.');
+        setLoading(false);
         console.error('Error al obtener la cantidad de productos disponibles:', error);
       });
-  }, []); 
+  }, []);
 
   return (
-    <>
-      <button className={styles.disponible} onClick={onClick}>
-        <span className={styles.span}>Productos Disponibles</span> {productosDisponibles}
-      </button>
-    </>
+    <button className={styles.disponible} onClick={onClick}>
+      {loading ? (
+        <span className={styles.span}>Cargando...</span>
+      ) : error ? (
+        <span className={styles.span}>{error}</span>
+      ) : (
+        <>
+          <span className={styles.span}>Productos Disponibles</span> {productosDisponibles}
+        </>
+      )}
+    </button>
   );
 };
 
