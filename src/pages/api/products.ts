d931b411840414b0148a0cf9c1d06103a7cf9ea3
Victor Peinado <vpeinado263@gmail.com>
2024-07-22 01:17:56 -0300

    probando endpoint

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -3,9 +3,14 @@ import axios from 'axios';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const response = await axios.get(''); // URL de tu API de backend
+    // URL de la API backend
+    const response = await axios.get('https://mi-back-end.onrender.com/products'); 
+    
+    // Enviar la respuesta de la API a la respuesta de la solicitud
     res.status(200).json(response.data);
   } catch (error) {
+    // Manejar errores de manera más informativa
+    console.error('Error al obtener productos:', error);
     res.status(500).json({ error: 'Failed to fetch products' });
   }
 };
