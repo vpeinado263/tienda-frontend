d0e27b0750e48b0ec4e7fdc249dd2b680dbe0d1a
Victor Peinado <vpeinado263@gmail.com>
2024-07-22 23:15:31 -0300

    vamos

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://mi-back-end.onrender.com',
-  timeout: 10000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
