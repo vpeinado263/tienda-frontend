fda70f8c4fa175ace22b1ce5367759cc33f39f1c
Victor Peinado <vpeinado263@gmail.com>
2024-07-24 10:33:49 -0300

    4

--- src/app/components/organisms/ProductList/ProductList.tsx
+++ src/app/components/organisms/ProductList/ProductList.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './ProductList.module.css';
-import ProductCard from '../../molecules/ProductCard/ProductCard';
+import ProductCard from '../../molecules/ProductCard/ProductCard'; // Asegúrate de que esta ruta es correcta
 
 interface Product {
   _id: string;
@@ -12,29 +12,33 @@ interface Product {
   quantity: number;
 }
 
-const ProductList = () => {
+const ProductList: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  const fetchProducts = async () => {
+  const fetchProducts = async (searchTerm = '') => {
     try {
-      const response = await axios.get('https://mi-back-end.onrender.com/api/products?search=${searchTerm}');
-      console.log(response.data);
-      setProducts(response.data);
+      setLoading(true);
+      const response = await axios.get(`https://mi-back-end.onrender.com/api/products?search=${searchTerm}`);
+      setProducts(response.data.data); // Asegúrate de que la estructura de los datos es correcta
+      setLoading(false);
     } catch (error) {
       setError('Error al cargar los productos. Por favor, inténtalo de nuevo más tarde.');
+      setLoading(false);
       console.error('Error fetching products:', error);
     }
   };
 
   return (
     <div className={styles.container}>
+      {loading && <p className={styles.info}>Cargando productos...</p>}
       {error && <p className={styles.error}>{error}</p>}
-      {products.length === 0 && !error && <p className={styles.info}>No hay productos disponibles.</p>}
+      {!loading && products.length === 0 && !error && <p className={styles.info}>No hay productos disponibles.</p>}
       <div className={styles.productGrid}>
         {products.map((product) => (
           <ProductCard key={product._id} product={product} />
@@ -45,4 +49,3 @@ const ProductList = () => {
 };
 
 export default ProductList;
-
