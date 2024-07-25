289964826fb9e9169e90721e686cdd73725d8bd3
Victor Peinado <vpeinado263@gmail.com>
2024-07-24 22:13:22 -0300

    vamos

--- src/app/components/organisms/ProductList/ProductList.tsx
+++ src/app/components/organisms/ProductList/ProductList.tsx
@@ -42,7 +42,7 @@ const ProductList: React.FC = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
+      <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
           <ProductCard key={product._id} product={product} />
