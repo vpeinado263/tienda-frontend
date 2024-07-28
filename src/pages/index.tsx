abef9c4a5f357433983341b5fa5d07f832b98131
Victor Peinado <vpeinado263@gmail.com>
2024-07-27 22:50:16 -0300

    correccion de errores

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../app/components/organisms/NavBar/NavBar';
 import Carousel from '../app/components/organisms/Carousel/Carousel';
-import WhatsapButton from '../app/components/atoms/WhatsappButton/WhatsappButton';
 import Footer from '../app/components/organisms/Footer/footer';
 import ServicesSection from '../app/components/organisms/ServicesSection/ServicesSection';
 
