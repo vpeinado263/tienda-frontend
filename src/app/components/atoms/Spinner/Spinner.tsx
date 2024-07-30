ed8112e9a82849938bab10d3daff52480a855a1a
Victor Peinado <vpeinado263@gmail.com>
2024-07-30 10:49:11 -0300

    cambios en el spinner

--- src/app/components/atoms/Spinner/Spinner.tsx
+++ src/app/components/atoms/Spinner/Spinner.tsx
@@ -6,7 +6,7 @@ const Spinner = () => {
     <div className={styles.spinnerContainer}>
       <div className={styles.spinner}>
       </div>
-      <p className={styles.loadingText}>Cargando, por favor espera...</p>
+      <p className={styles.loadingText}>Cargando...</p>
     </div>
   );
 };
