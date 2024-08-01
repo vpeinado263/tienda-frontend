1a87537b9d40ceec28554b628958ba715f938bf6
Victor Peinado <vpeinado263@gmail.com>
2024-07-31 21:03:12 -0300

    mas cambios

--- src/app/components/templates/ImageUpload.tsx
+++ src/app/components/templates/ImageUpload.tsx
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Image from 'next/image';
 
 const ImageUpload = () => {
   const [file, setFile] = useState<File | null>(null);
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const [imageUrl, setImageUrl] = useState<string>('');
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      setFile(event.target.files[0]);
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files[0]);
     }
   };
 
-  const handleUpload = async () => {
+  const onUpload = async () => {
     if (!file) {
-      setError('Por favor, selecciona un archivo para subir.');
+      alert('Por favor, selecciona un archivo primero.');
       return;
     }
 
@@ -25,38 +23,22 @@ const ImageUpload = () => {
     try {
       const response = await axios.post('https://mi-back-end.onrender.com/api/upload', formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        timeout: 30000,
+          'Content-Type': 'multipart/form-data'
+        }
       });
 
-      if (response.status === 200 && response.data.url) {
-        console.log('Imagen subida con éxito:', response.data);
-        setImageUrl(response.data.url);
-        setError(null);
-      } else {
-        console.error('Error inesperado al subir la imagen:', response.data);
-        setError('Error inesperado al subir la imagen.');
-      }
-    } catch (err) {
-      console.error('Error al subir la imagen:', err);
-      if (axios.isAxiosError(err) && err.response) {
-        setError(`Error al subir la imagen: ${err.response.data.message}`);
-      } else {
-        setError('Error al subir la imagen. Por favor, inténtalo de nuevo más tarde.');
-      }
+      setImageUrl(response.data.file.secure_url);
+    } catch (error) {
+      console.error('Error al subir el archivo:', error);
+      alert('Hubo un error al subir el archivo.');
     }
   };
 
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Subir Imagen</button>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {imageUrl && 
-      <Image src={imageUrl} 
-      alt="Uploaded" 
-      style={{ marginTop: '10px', maxWidth: '100%' }} />}
+      <input type="file" onChange={onFileChange} />
+      <button onClick={onUpload}>Subir Imagen</button>
+      {imageUrl && <img src={imageUrl} alt="Imagen subida" />}
     </div>
   );
 };
