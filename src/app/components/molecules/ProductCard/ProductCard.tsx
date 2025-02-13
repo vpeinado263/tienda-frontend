a61cd16b9dbc24b5b4bd88479c68ebf9d1afba2b
Victor Peinado <vpeinado263@gmail.com>
2025-02-13 01:47:59 -0300

    Realizar modificaciones a tarjetas

--- src/app/components/molecules/ProductCard/ProductCard.tsx
+++ src/app/components/molecules/ProductCard/ProductCard.tsx
@@ -11,15 +11,11 @@ function ProductCard({ product }: Props) {
   const whatsappMessage = `¿Hola, está disponible?: ${product.name}`;
 
   return (
-    <div className="w-full max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-      <div className="p-6">
+    <div>
+      <div className="p-2">
         <h2 className="text-xl font-bold mb-2">{product.name}</h2>
         <p className="text-gray-700 mb-4">Precio: ${product.price}</p>
-        {product.imageUrls && product.imageUrls.length > 0 && (
-          <div>
-            <ImageCarousel images={product.imageUrls} />
-          </div>
-        )}
+        {product.imageUrls && product.imageUrls.length > 0 && (<ImageCarousel images={product.imageUrls} />)}
         <div className="mt-4">
           <p className="text-gray-800 mb-2">Descripción: {product.description}</p>
           <p className="text-gray-800 mb-4">Cantidad disponible: {product.quantity}</p>
