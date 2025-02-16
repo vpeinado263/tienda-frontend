b73bd013854f7bbd855ea336387987fdd867f5c1
Victor Peinado <vpeinado263@gmail.com>
2025-02-16 18:46:25 -0300

    Configurar botones para que estos se oculten en pantallas pequeñas

--- src/app/components/atoms/AgregarProductoButton/AgregarProductoButton.tsx
+++ src/app/components/atoms/AgregarProductoButton/AgregarProductoButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AgregarProductoButton = () => {
   return (
-    <div>
+    <div className="hidden md:block">
      <button className="bg-blue-600 text-white rounded-md text-base font-semibold py-2 px-4 shadow-md transition-transform transform hover:scale-105 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 
      sm:py-2 sm:px-4 md:py-3 md:px-6 lg:py-4 lg:px-8
      sm:text-sm md:text-base lg:text-lg
