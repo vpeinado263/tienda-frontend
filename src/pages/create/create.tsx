469101811b85e705064fbb09d04f59296318766d
Victor Peinado <vpeinado263@gmail.com>
2025-02-18 16:45:11 -0300

    Restaurar archivos

---
+++ src/pages/create/create.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CreateProductForm from "../../app/components/molecules/CreateProductForm/CreateProductForm";
+const create = () => {
+    return (
+        <>
+          <CreateProductForm/>
+        </>
+    );
+};
+export default create;
---
+++ src/pages/products/productPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductList from "../../app/components/organisms/ProductList/ProductList";
+const ProductsPage = () => {
+    return (
+        <>
+        <ProductList/>
+        </>
+    );
+};
+export default ProductsPage;
