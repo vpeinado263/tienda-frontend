2d3adf1abbe8c410bcb576e8b07c4bf5f8cb8b88
Victor Peinado <vpeinado263@gmail.com>
2025-02-19 21:21:51 -0300

    Mofificar componente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
-import AgregarProductoButton from '../app/components/atoms/AgregarProductoButton/AgregarProductoButton';
 import ProductList from '../app/components/organisms/ProductList/ProductList';
 import NavBar from '../app/components/templates/NavBar/NavBar';
 
@@ -10,11 +8,6 @@ const Home = () => {
     <>
       <NavBar />
       <main>
-      <div className="flex items-center">
-        <Link href="/create/create" passHref>
-            <AgregarProductoButton />
-        </Link>
-      </div>
       <ProductList />
     </main>
     </>
