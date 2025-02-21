ef8a4600c82f815812e27e710e5b4fe8ec839e2c
Victor Peinado <vpeinado263@gmail.com>
2025-02-21 16:55:36 -0300

    Agregar cambios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import 'tailwindcss/tailwind.css'; 
 import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
     <>
-     <Component {...pageProps} />
-     <ToastContainer />
+      <Component {...pageProps} />
+      <ToastContainer />
+      <SpeedInsights /> 
     </>
   );
 }
 
 export default MyApp;
+
