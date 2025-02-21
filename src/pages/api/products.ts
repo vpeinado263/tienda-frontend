03ee1d1589d0f79e092ac343962fd6a0e69472d8
Victor Peinado <vpeinado263@gmail.com>
2025-02-21 16:06:04 -0300

    Modificar api/products

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -4,14 +4,19 @@ import axios from 'axios';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const response = await axios.get('https://mi-back-end.onrender.com/api/products');
-    res.status(200).json(response.data);
+    
+    if (Array.isArray(response.data)) {
+      res.status(200).json({ count: response.data.length }); 
+    } else {
+      res.status(500).json({ error: 'Unexpected API response format' });
+    }
   } catch (error) {
     if (axios.isAxiosError(error)) {
       const errorMessage = error.response?.data?.message || error.message;
       res.status(error.response?.status || 500).json({ error: errorMessage });
     } else {
       console.error('Unexpected error:', error);
-      res.status(500).json({ error: 'Error al cargar Productos' });
+      res.status(500).json({ error: 'Error al cargar productos' });
     }
   }
 };
