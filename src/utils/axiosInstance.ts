a6efd09afe9fb65e8880393926475c9280dd1a9b
Victor Peinado <vpeinado263@gmail.com>
2025-02-22 21:16:27 -0300

    Agrgar mas tiempo de carga a spinner

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://mi-back-end.onrender.com',
-  timeout: 10000, 
+  timeout: 1000, 
   headers: {
     'Content-Type': 'application/json',
   },
