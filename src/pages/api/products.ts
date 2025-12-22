747acd87a978b366cf528e44321c74aa5fee74ba
Victor Peinado <vpeinado263@gmail.com>
2025-12-21 22:42:24 -0300

    Modificar archivos de configuración

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -3,9 +3,7 @@ import axios from "axios";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const response = await axios.get(
-      `${process.env.BACKEND_URL}/api/products`
-    );    
+    const response = await axios.get(`${process.env.BACKEND_URL}/api/products`);
 
     if (Array.isArray(response.data)) {
       res.status(200).json({ count: response.data.length });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,4 +22,3 @@ const contentConfig = {
 };
 
 export default contentConfig;
-
